22ab5a0235c09e7d2c18bb704dae67f12d4cab83
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-11 11:59:55 +0200

    created constructor in Bicycle class

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -2,4 +2,10 @@ class Bicycle {
     String producer;
     String model;
     int size;
+
+    public Bicycle(String producer, String model, int size) {
+        this.producer = producer;
+        this.model = model;
+        this.size = size;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        Bicycle bicycle1 = new Bicycle();
-        bicycle1.producer = "Scott";
-        bicycle1.model = "Speedster";
-        bicycle1.size = 52;
-
-        Bicycle bicycle2 = new Bicycle();
-        bicycle2.producer = "Merida";
-        bicycle2.model = "Scultura";
-        bicycle2.size = 49;
+        Bicycle bicycle1 = new Bicycle("Scott", "Speedster", 52);
+        Bicycle bicycle2 = new Bicycle("Merida", "Scultura", 49);
 
         System.out.println(bicycle1.producer + " " + bicycle1.model + ", rozmiar: " + bicycle1.size);
         System.out.println(bicycle2.producer + " " + bicycle2.model + ", rozmiar: " + bicycle2.size);
